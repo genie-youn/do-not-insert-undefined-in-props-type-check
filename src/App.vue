578afe3229d87dcdf24677012ab8c93631e40be4
genie-youn <yjs930915@gmail.com>
2020-06-12 07:19:30 +0900

    project init

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <img alt="Vue logo" src="src/assets/logo.png" />
+  <HelloWorld :msg="msg" />
+  <button @click="setUndefined">set undefined</button>
+  <button @click="setObject">set Object</button>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data() {
+    return {
+      msg: ""
+    }
+  },
+  methods: {
+    setUndefined() {
+      console.log("undefined");
+      this.msg = undefined;
+    },
+    setObject() {
+      console.log("object");
+      this.msg = {
+        test: "abc",
+      };
+    },
+  },
+}
+</script>
